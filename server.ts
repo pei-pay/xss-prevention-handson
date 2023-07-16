37cde7fb5c93a0d3935ae1ecd73004501b3655cf
pei-pay <gleektennis@icloud.com>
2023-07-16 10:55:57 +0900

    feat: serveStatic html pages

--- server.ts
+++ server.ts
@@ -1,8 +1,16 @@
-import { Hono } from 'https://deno.land/x/hono@v3.3.0/mod.ts';
 import { serveStatic } from 'https://deno.land/x/hono@v3.3.0/middleware.ts';
+import { Hono } from 'https://deno.land/x/hono@v3.3.0/mod.ts';
 
 const app = new Hono();
 
-app.get('/', serveStatic({ path: './public/index.html' }));
+app.get(
+  '/*',
+  serveStatic({
+    root: './public',
+    rewriteRequestPath: (path) => {
+      return path.concat('.html');
+    },
+  })
+);
 
 Deno.serve({ port: 8787 }, app.fetch);
